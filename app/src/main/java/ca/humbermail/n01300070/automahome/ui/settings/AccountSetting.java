2e611b81244695ec777900c6c1e0a8f574bc33b2
nthie <n01287624@humbermail.ca>
2020-11-08 11:56:39 -0500

    Y-complete layout for Setting but not yet navigate

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/settings/AccountSetting.java
@@ -0,0 +1,16 @@
+package ca.humbermail.n01300070.automahome.ui.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.humbermail.n01300070.automahome.R;
+
+public class AccountSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_setting);
+    }
+}

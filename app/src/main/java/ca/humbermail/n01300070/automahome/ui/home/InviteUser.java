399c841844f54f7a71a69cb75b1c5c1b482f5080
nthie <n01287624@humbermail.ca>
2020-11-07 23:34:14 -0500

    Y-Home commit

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/home/InviteUser.java
@@ -0,0 +1,16 @@
+package ca.humbermail.n01300070.automahome.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.humbermail.n01300070.automahome.R;
+
+public class InviteUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_user);
+    }
+}

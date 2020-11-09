57edb8a549742134451f130e14cb112d8b6869bb
DESKTOP-510I2Q4\Rohan <rohanmohmmir@gmail.com>
2020-11-09 14:38:31 -0500

    Added task activities for Edit, Lights, and Schedule (Mostly XML, no running code yet)

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/tasks/EditTask.java
@@ -0,0 +1,27 @@
+package ca.humbermail.n01300070.automahome.ui.tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ListMenuItemView;
+
+import ca.humbermail.n01300070.automahome.R;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class EditTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+
+
+
+    }
+}
---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/tasks/TaskLights.java
@@ -0,0 +1,14 @@
+package ca.humbermail.n01300070.automahome.ui.tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ca.humbermail.n01300070.automahome.R;
+import android.os.Bundle;
+
+public class TaskLights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_lights);
+    }
+}

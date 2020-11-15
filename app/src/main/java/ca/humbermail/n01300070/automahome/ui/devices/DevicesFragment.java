e03de3f2c3235799ddbb63032230e5ae54d07267
Calvin Abel <n01300070@humbermail.ca>
2020-11-15 15:07:21 -0500

    Changed OnClickHandler for list items in DevicesFragment to open ControlDeviceActivity

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/DevicesFragment.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/DevicesFragment.java
@@ -1,19 +1,16 @@
 package ca.humbermail.n01300070.automahome.ui.devices;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,6 +19,8 @@ import ca.humbermail.n01300070.automahome.R;
 import ca.humbermail.n01300070.automahome.components.CategorizedDeviceOrTaskButtonRecyclerViewAdapter;
 import ca.humbermail.n01300070.automahome.components.DeviceOrTaskButtonView;
 import ca.humbermail.n01300070.automahome.components.RecyclerViewCategoryPadding;
+import ca.humbermail.n01300070.automahome.data.model.DeviceOrTaskData;
+import ca.humbermail.n01300070.automahome.ui.devices.control.ControlDevicesActivity;
 
 public class DevicesFragment extends Fragment
 {
@@ -33,8 +32,8 @@ public class DevicesFragment extends Fragment
 	private CategorizedDeviceOrTaskButtonRecyclerViewAdapter categoryAdapter;
 	private View.OnClickListener categoryOnClickListener;
 	
-	public View onCreateView(@NonNull LayoutInflater inflater,
-							 ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(@NonNull final LayoutInflater inflater,
+							 final ViewGroup container, Bundle savedInstanceState) {
 		DevicesViewModel devicesViewModel = new ViewModelProvider(this).get(DevicesViewModel.class);
 		View root = inflater.inflate(R.layout.fragment_devices, container, false);
 		context = getActivity().getApplicationContext();
@@ -48,9 +47,13 @@ public class DevicesFragment extends Fragment
 		categoryOnClickListener = new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				// TODO: Replace placeholder on click function with navigation to the correct Control Device or Edit Task Activity
 				DeviceOrTaskButtonView deviceOrTaskButtonView = (DeviceOrTaskButtonView) view;
-				deviceOrTaskButtonView.setExtraText("Clicked");
+				Intent intent = new Intent();
+				
+				intent.setClass(context, ControlDevicesActivity.class);
+				intent.putExtra(DeviceOrTaskData.ARG_DEVICE, deviceOrTaskButtonView.getDeviceType());
+				
+				startActivity(intent);
 			}
 		};
 		categoryAdapter = new CategorizedDeviceOrTaskButtonRecyclerViewAdapter( context,

841b3601d2d3905274175644eb0fe81bb681f8d2
Calvin Abel <n01300070@humbermail.ca>
2020-11-15 13:06:01 -0500

    Added back button functionality to ControlDevicesActivity and EditDevicesActivity

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlDevicesActivity.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlDevicesActivity.java
@@ -1,8 +1,10 @@
 package ca.humbermail.n01300070.automahome.ui.devices.control;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import ca.humbermail.n01300070.automahome.R;
 
@@ -12,5 +14,17 @@ public class ControlDevicesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_devices);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    
+    /**
+     * Handles back button onClick event
+     * @param item non-null MenuItem
+     * @return Boolean
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        finish();
+        return true;
     }
 }

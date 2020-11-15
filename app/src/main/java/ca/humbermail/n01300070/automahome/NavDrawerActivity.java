d4c753d8b2e657460e01a67f4abe462cffc095e8
Calvin Abel <n01300070@humbermail.ca>
2020-11-14 19:35:57 -0500

    Fixed typos

--- app/src/main/java/ca/humbermail/n01300070/automahome/NavDrawerActivity.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/NavDrawerActivity.java
@@ -217,9 +217,9 @@ public class NavDrawerActivity extends AppCompatActivity {
 		}
 	}
 
-	ImageButton celciusButton;
+	ImageButton celsiusButton;
 
-	public void celciusBtn_Clicked(View view){
+	public void celsiusButton_Clicked(View view){
 
 		if (findViewById(R.id.nav_host_fragment) != null) {
 			// Set the Main Fragment
@@ -232,9 +232,9 @@ public class NavDrawerActivity extends AppCompatActivity {
 		}
 	}
 
-	ImageButton farenheitButton;
+	ImageButton fahrenheitButton;
 
-	public void farenheitBtn_Clicked(View view){
+	public void fahrenheitButton_Clicked(View view){
 
 		if (findViewById(R.id.nav_host_fragment) != null) {
 			// Set the Main Fragment

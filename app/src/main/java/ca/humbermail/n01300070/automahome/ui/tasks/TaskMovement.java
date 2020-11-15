ec96371deff4871c6f57d1b84738926ac5538298
DESKTOP-510I2Q4\Rohan <rohanmohmmir@gmail.com>
2020-11-14 22:43:03 -0500

    Added XML for all task activities

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/tasks/TaskMovement.java
@@ -0,0 +1,16 @@
+package ca.humbermail.n01300070.automahome.ui.tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.humbermail.n01300070.automahome.R;
+
+public class TaskMovement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_movement);
+    }
+}

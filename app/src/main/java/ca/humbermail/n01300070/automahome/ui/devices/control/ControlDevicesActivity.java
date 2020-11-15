268493c072a39a3eb64e612410fff22d0e6e5ffb
Jay Patel <n01298337@humbermail.ca>
2020-11-15 12:17:30 -0500

    Classes for ControlDevicesActivity and EditDevicesActivity

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlDevicesActivity.java
@@ -0,0 +1,16 @@
+package ca.humbermail.n01300070.automahome.ui.devices.control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.humbermail.n01300070.automahome.R;
+
+public class ControlDevicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_devices);
+    }
+}

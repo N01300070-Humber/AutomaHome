4433262ecb38cf44ab63385278a78d830c43d28f
Calvin Abel <n01300070@humbermail.ca>
2020-11-16 16:47:14 -0500

    Finished NotificationSettingsFragment code and layout

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/settings/NotificationSettingsFragment.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/settings/NotificationSettingsFragment.java
@@ -1,17 +1,23 @@
 package ca.humbermail.n01300070.automahome.ui.settings;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import ca.humbermail.n01300070.automahome.R;
 
 public class NotificationSettingsFragment extends Fragment {
+    Context context;
+    
+    SwitchMaterial showErrorNotificationSwitch;
     
     public NotificationSettingsFragment() {
         // Required empty public constructor
@@ -26,7 +32,20 @@ public class NotificationSettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_settings_notification, container, false);
+    
+        showErrorNotificationSwitch = root.findViewById(R.id.switch_settings_showErrorNotifications);
+        
+        showErrorNotificationSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showErrorNotificationSwitch_Toggled(view);
+            }
+        });
         
         return root;
+    }
+    
+    public void showErrorNotificationSwitch_Toggled(View view){
+        // TODO: change errorNotification setting in user preferences
     }
 }

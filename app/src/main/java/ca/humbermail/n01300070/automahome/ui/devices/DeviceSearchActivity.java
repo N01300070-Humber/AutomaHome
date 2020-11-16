847fddb7eaca6d0b91a00c6c5736cb3262333f4b
Calvin Abel <n01300070@humbermail.ca>
2020-11-15 23:10:41 -0500

    Created DeviceSearchActivity activity code

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/DeviceSearchActivity.java
@@ -0,0 +1,117 @@
+package ca.humbermail.n01300070.automahome.ui.devices;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import ca.humbermail.n01300070.automahome.NavDrawerActivity;
+import ca.humbermail.n01300070.automahome.R;
+import ca.humbermail.n01300070.automahome.components.CategorizedDeviceOrTaskButtonRecyclerViewAdapter;
+import ca.humbermail.n01300070.automahome.components.DeviceOrTaskButtonRecyclerViewAdapter;
+import ca.humbermail.n01300070.automahome.components.DeviceOrTaskButtonView;
+import ca.humbermail.n01300070.automahome.components.RecyclerViewItemDivider;
+import ca.humbermail.n01300070.automahome.data.model.DeviceOrTaskData;
+import ca.humbermail.n01300070.automahome.ui.devices.edit.EditDevicesActivity;
+
+public class DeviceSearchActivity extends AppCompatActivity {
+	
+	int REQUEST_NEW_DEVICE = 2;
+	
+	private ProgressBar searchProgress;
+	
+	private RecyclerView recyclerView;
+	
+	private DeviceOrTaskButtonRecyclerViewAdapter resultsAdapter;
+	private View.OnClickListener resultsOnClickListener;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_device_search);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		recyclerView = findViewById(R.id.recyclerView_deviceSearch_results);
+		
+		resultsOnClickListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				DeviceOrTaskButtonView deviceOrTaskButtonView = (DeviceOrTaskButtonView) view;
+				
+				Intent intent = new Intent(getApplicationContext(), EditDevicesActivity.class);
+				intent.putExtra(DeviceOrTaskData.ARG_DEVICE, deviceOrTaskButtonView.getDeviceType());
+				startActivityForResult(intent, REQUEST_NEW_DEVICE);
+			}
+		};
+		resultsAdapter = new DeviceOrTaskButtonRecyclerViewAdapter(getApplicationContext(), generateDeviceResultsDataList(), resultsOnClickListener);
+		
+		recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 1));
+		recyclerView.setAdapter(resultsAdapter);
+		recyclerView.addItemDecoration(new RecyclerViewItemDivider(
+				(int) getResources().getDimension(R.dimen.recycler_divider_space),
+				(int) getResources().getDimension(R.dimen.activity_vertical_margin),
+				(int) getResources().getDimension(R.dimen.activity_horizontal_margin)
+		));
+	}
+	
+	// TODO: Replace with method that obtains real data
+	private ArrayList<DeviceOrTaskData> generateDeviceResultsDataList() {
+		Random random = new Random();
+		int listLength = random.nextInt(10) + 1;
+		ArrayList<DeviceOrTaskData> deviceResultsDataList = new ArrayList<>(listLength);
+		String[] deviceNames = {"Name of Light", "Name of Movement Sensor", "Name of Thermostat"};
+		String[] deviceTypes = {DeviceOrTaskData.DEVICE_LIGHTS,
+				DeviceOrTaskData.DEVICE_MOVEMENT_SENSOR, DeviceOrTaskData.DEVICE_THERMOSTAT};
+		
+		for (int i = 0; i < listLength; i++) {
+			int randInt = random.nextInt(3);
+			
+			DeviceOrTaskData deviceData = new DeviceOrTaskData(
+					DeviceOrTaskData.TYPE_DEVICE,
+					deviceNames[randInt],
+					"Room",
+					ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_devices),
+					getString(R.string.content_description_type_device),
+					getColor(R.color.accent_200)
+			);
+			deviceData.setDeviceType(deviceTypes[randInt]);
+			
+			deviceResultsDataList.add(deviceData);
+		}
+		
+		return deviceResultsDataList;
+	}
+	
+	/**
+	 * Handles back button onClick event
+	 * @param item non-null MenuItem
+	 * @return Boolean
+	 */
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		finish();
+		return true;
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == REQUEST_NEW_DEVICE) {
+			if (resultCode == RESULT_OK) {
+				finish();
+			}
+		}
+	}
+}

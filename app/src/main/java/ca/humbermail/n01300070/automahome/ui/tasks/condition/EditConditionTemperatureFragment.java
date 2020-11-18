f34c7313e43ecaf31245478c61e3858d9e0e22b0
Calvin Abel <n01300070@humbermail.ca>
2020-11-17 22:12:48 -0500

    Created variables for components in layout and added placeholder data to temperature picker

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/tasks/condition/EditConditionTemperatureFragment.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/tasks/condition/EditConditionTemperatureFragment.java
@@ -1,5 +1,6 @@
 package ca.humbermail.n01300070.automahome.ui.tasks.condition;
 
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
@@ -7,11 +8,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import ca.humbermail.n01300070.automahome.R;
 
 public class EditConditionTemperatureFragment extends Fragment {
     private Context context;
+    
+    TextInputEditText temperatureEditText;
+    TextView temperatureUnitTextView;
+    AppCompatImageView increaseTemperatureButton;
+    AppCompatImageView decreaseTemperatureButton;
+    MaterialButton logicLessThanButton;
+    MaterialButton logicEqualToButton;
+    MaterialButton logicMoreThanButton;
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +33,17 @@ public class EditConditionTemperatureFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_edit_condition_temperature, container, false);
         context = getActivity().getApplicationContext();
+        
+        temperatureEditText = root.findViewById(R.id.textInputEditText_temperature);
+        temperatureUnitTextView = root.findViewById(R.id.textView_editConditionTemperature_unit);
+        increaseTemperatureButton = root.findViewById(R.id.imageView_editConditionTemperature_increase);
+        decreaseTemperatureButton = root.findViewById(R.id.imageView_editConditionTemperature_decrease);
+        logicLessThanButton = root.findViewById(R.id.toggleButton_lessThan);
+        logicEqualToButton = root.findViewById(R.id.toggleButton_equal);
+        logicMoreThanButton = root.findViewById(R.id.toggleButton_moreThan);
+        
+        temperatureEditText.setText("32"); // TODO: Replace hardcoded text with saved temperature or default (convert if user preference is fahrenheit)
+        temperatureUnitTextView.setText(getString(R.string.degrees_celsius)); // TODO: Replace getString(resource) with unit preference from user settings
         
         return root;
     }

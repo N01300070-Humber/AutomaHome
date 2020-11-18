767c1334ef5f3afb74a5ad3a734f80184b0dd75f
Calvin Abel <n01300070@humbermail.ca>
2020-11-17 20:18:53 -0500

    Added onClick handling code to buttons in AddNetworkActivity

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/manageHome/AddNetworkActivity.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/manageHome/AddNetworkActivity.java
@@ -4,39 +4,63 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
-import android.view.LayoutInflater;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import ca.humbermail.n01300070.automahome.R;
 
 public class AddNetworkActivity extends AppCompatActivity {
-    
-    EditText networkEditText;
-    Button addButton;
-    
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_network);
-    }
-    
-    /**
-     * onClick event handler for AddNetworkButton
-     * @param view Source view
-     */
-    public void addNetworkBtn_Clicked(View view){
-        addNetwork();
-    }
-    
-    public void addNetwork() {
-        // TODO: Add network to Wi-Fi Detection list of current home if not already in use by another home
-        Toast.makeText(getApplicationContext(), "Network Added", Toast.LENGTH_SHORT).show();
-        finish();
-    }
+	
+	EditText networkEditText;
+	Button addNetworkBySSIDButton;
+	Button addCurrentNetworkButton;
+	
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_network);
+		
+		networkEditText = findViewById(R.id.textInputEditText_ssid);
+		addNetworkBySSIDButton = findViewById(R.id.button_addNetworkFromSSID);
+		addCurrentNetworkButton = findViewById(R.id.button_addNetworkFromCurrent);
+		
+		networkEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+				addNetworkBySSID();
+				return true;
+			}
+		});
+	}
+	
+	/**
+	 * onClick event handler for addNetworkBySSIDButton
+	 * @param view Source view
+	 */
+	public void addNetworkBySSIDButton_Clicked(View view) {
+		addNetworkBySSID();
+	}
+	
+	/**
+	 * onCLick event handler for addCurrentNetworkButton
+	 * @param view Source view
+	 */
+	public void addCurrentNetworkButton_Clicked(View view) {
+		// TODO: Add currently connected network to Wi-Fi Detection list of current home if not already in use by another home
+		Toast.makeText(getApplicationContext(), "Current network added", Toast.LENGTH_SHORT).show();
+		finish();
+	}
+	
+	public void addNetworkBySSID() {
+		String ssid = networkEditText.getText().toString();
+		
+		// TODO: Add network from provided SSID to Wi-Fi Detection list of current home if not already in use by another home
+		Toast.makeText(getApplicationContext(), "Network " + ssid + " added", Toast.LENGTH_SHORT).show();
+		finish();
+	}
 }

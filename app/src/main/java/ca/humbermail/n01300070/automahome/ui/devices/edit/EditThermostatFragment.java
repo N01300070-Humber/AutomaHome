1b29ca670303e79f995c94aa20e0bf96311c103a
Calvin Abel <n01300070@humbermail.ca>
2020-11-18 07:35:51 -0500

    Changed layout for EditThermostatFragment to fit the style of the rest and use NumberSelectorView for getting temperature

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/edit/EditThermostatFragment.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/edit/EditThermostatFragment.java
@@ -11,47 +11,41 @@ import android.widget.ToggleButton;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 import ca.humbermail.n01300070.automahome.R;
+import ca.humbermail.n01300070.automahome.components.NumberPickerView;
 
 public class EditThermostatFragment extends Fragment {
-
-    private TextView currentTemp;
-    private TextView targetTemp;
-    private ToggleButton heat;
-    private ToggleButton cool;
-    private Button up;
-    private Button down;
-    private Button set;
-    private Context context;
-
-    public EditThermostatFragment()
-    {
-        // Required empty public constructor
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_edit_device_thermostat, container, false);
-        context = getActivity().getApplicationContext();
-
-        currentTemp = root.findViewById(R.id.textView_thermoTempSensor_currentTemp);
-        targetTemp = root.findViewById(R.id.textView_thermoTempSensor_targetTemp);
-
-        heat = root.findViewById(R.id.toggleButton_thermoTempSensor_heat);
-        cool = root.findViewById(R.id.toggleButton_thermoTempSensor_cool);
-
-        up = root.findViewById(R.id.Button_thermoTempSensor_upArrow);
-        down = root.findViewById(R.id.Button_thermoTempSensor_downArrow);
-        set = root.findViewById(R.id.Button_thermoTempSensor_setButton);
-
-        //TODO replace hardcoded strings for actual readings
-        currentTemp.setText("23°C");
-        targetTemp.setText("28°C");
-
-        return root;
-    }
-
+	private Context context;
+	
+	private TextView currentTemperatureTextView;
+	private NumberPickerView targetTemperatureNumberPickerView;
+	private MaterialButton heatingButton;
+	private MaterialButton coolingButton;
+	
+	public EditThermostatFragment() {
+		// Required empty public constructor
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View root = inflater.inflate(R.layout.fragment_edit_device_thermostat, container, false);
+		context = getActivity().getApplicationContext();
+		
+		currentTemperatureTextView = root.findViewById(R.id.textView_editThermostat_currentTemp);
+		targetTemperatureNumberPickerView = root.findViewById(R.id.numberPickerView_editThermostat_targetTemp);
+		heatingButton = root.findViewById(R.id.toggleButton_editThermostat_heating);
+		coolingButton = root.findViewById(R.id.toggleButton_editThermostat_cooling);
+		
+		//TODO replace hardcoded values for actual readings
+		currentTemperatureTextView.setText(23 + getString(R.string.degrees_celsius));
+		targetTemperatureNumberPickerView.setNumber(28);
+		targetTemperatureNumberPickerView.setSuffixText(getString(R.string.degrees_celsius));
+		
+		return root;
+	}
+	
 }

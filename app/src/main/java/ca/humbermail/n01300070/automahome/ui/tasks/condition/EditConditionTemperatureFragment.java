c72bdf05000ca7f3b6e81d5bf0163bb6d81ce7fc
Calvin Abel <n01300070@humbermail.ca>
2020-11-18 02:49:47 -0500

    Replaced the TextInputLayout, unitTextView, and ImageButtons with a NumberPickerView

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/tasks/condition/EditConditionTemperatureFragment.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/tasks/condition/EditConditionTemperatureFragment.java
@@ -8,21 +8,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 import ca.humbermail.n01300070.automahome.R;
+import ca.humbermail.n01300070.automahome.components.NumberPickerView;
 
 public class EditConditionTemperatureFragment extends Fragment {
     private Context context;
     
-    TextInputEditText temperatureEditText;
-    TextView temperatureUnitTextView;
-    AppCompatImageView increaseTemperatureButton;
-    AppCompatImageView decreaseTemperatureButton;
+    NumberPickerView temperatureNumberPickerView;
     MaterialButton logicLessThanButton;
     MaterialButton logicEqualToButton;
     MaterialButton logicMoreThanButton;
@@ -34,16 +31,13 @@ public class EditConditionTemperatureFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_edit_condition_temperature, container, false);
         context = getActivity().getApplicationContext();
         
-        temperatureEditText = root.findViewById(R.id.textInputEditText_temperature);
-        temperatureUnitTextView = root.findViewById(R.id.textView_editConditionTemperature_unit);
-        increaseTemperatureButton = root.findViewById(R.id.imageView_editConditionTemperature_increase);
-        decreaseTemperatureButton = root.findViewById(R.id.imageView_editConditionTemperature_decrease);
+        temperatureNumberPickerView = root.findViewById(R.id.numberPickerView_editConditionTemperature);
         logicLessThanButton = root.findViewById(R.id.toggleButton_lessThan);
         logicEqualToButton = root.findViewById(R.id.toggleButton_equal);
         logicMoreThanButton = root.findViewById(R.id.toggleButton_moreThan);
-        
-        temperatureEditText.setText("32"); // TODO: Replace hardcoded text with saved temperature or default (convert if user preference is fahrenheit)
-        temperatureUnitTextView.setText(getString(R.string.degrees_celsius)); // TODO: Replace getString(resource) with unit preference from user settings
+    
+        temperatureNumberPickerView.setNumber(32); // TODO: Replace hardcoded number with saved temperature or default (convert if user preference is fahrenheit)
+        temperatureNumberPickerView.setSuffixText(getString(R.string.degrees_celsius)); // TODO: Replace getString(resource) with unit preference from user settings
         
         return root;
     }

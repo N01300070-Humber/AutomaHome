ab1e1531b11683574a9b1de44b6d179a5427597f
Calvin Abel <n01300070@humbermail.ca>
2020-11-18 16:47:37 -0500

    Added FavoriteSelectView to EditDevicesActivity and filled labels with placeholders

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/edit/EditDevicesActivity.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/edit/EditDevicesActivity.java
@@ -13,14 +13,16 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import ca.humbermail.n01300070.automahome.R;
+import ca.humbermail.n01300070.automahome.components.FavoriteSelectView;
 import ca.humbermail.n01300070.automahome.data.model.DeviceOrTaskData;
 
 public class EditDevicesActivity extends AppCompatActivity
 {
     private Fragment fragment;
-    private Button editDeviceButton;
-
+    private FavoriteSelectView favoriteSelectView;
     private Spinner roomSpinner;
     private Spinner roomSpinner2;
     private TextView roomLocationHeader;
@@ -38,8 +40,10 @@ public class EditDevicesActivity extends AppCompatActivity
         roomSpinner2 = findViewById(R.id.spinner_editDevice_2);
         roomLocationHeader = findViewById(R.id.textView_deviceLocation_editDevice);
         roomLocationHeader2 = findViewById(R.id.textView_editDevice_deviceLocation2);
+        favoriteSelectView = findViewById(R.id.favoriteSelectView_editDevice);
 
         deviceType = getIntent().getExtras().getString(DeviceOrTaskData.ARG_DEVICE);
+        favoriteSelectView.setAutoCompleteLabels(generateCategoryList());
 
         switch(deviceType){
             case DeviceOrTaskData.DEVICE_LIGHTS:
@@ -74,6 +78,17 @@ public class EditDevicesActivity extends AppCompatActivity
         Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show(); // TODO: Remove placeholder toast
         setResult(Activity.RESULT_OK);
         finish();
+    }
+    
+    private ArrayList<String> generateCategoryList() {
+        int numCategories = 6;
+        ArrayList<String> categoryList = new ArrayList<>(numCategories);
+        
+        for (int i = 0; i < numCategories; i++) {
+            categoryList.add("Category " + (i + 1));
+        }
+        
+        return categoryList;
     }
     
     /**

ff50e81a9021d66dd8eaa6547e2b9256d83cda6e
Calvin Abel <n01300070@humbermail.ca>
2020-12-10 16:37:20 -0500

    Created DeviceAndTaskDataSource for connecting with the database

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/data/DeviceAndTaskDataSource.java
@@ -0,0 +1,11 @@
+package ca.humbermail.n01300070.automahome.data;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DeviceAndTaskDataSource {
+	private final static String HOMES_REFERENCE = "homes";
+	
+	
+	FirebaseDatabase database = FirebaseDatabase.getInstance();
+	
+}

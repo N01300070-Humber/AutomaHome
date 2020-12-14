c551c9dd86210eecb391138176ddb0c1e35602c0
Calvin Abel <n01300070@humbermail.ca>
2020-12-14 12:20:48 -0500

    Added conditionOrOperationId as a field in ConditionOrOperationView and it's data model so it can be accessed from an onClick function

--- app/src/main/java/ca/humbermail/n01300070/automahome/data/model/ConditionOrOperationViewData.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/data/model/ConditionOrOperationViewData.java
@@ -14,34 +14,41 @@ public class ConditionOrOperationViewData {
 	public static final String OPERATION_THERMOSTAT = "Thermostat";
 	
 	
+	private String conditionOrOperationId;
 	private int type;
 	private String conditionOrOperationType;
 	private String mainText;
 	private String typeText;
 	private boolean dragHandleVisible;
 	
-	public ConditionOrOperationViewData() {
-		this(-1, null, null, null, true);
+	public ConditionOrOperationViewData(int type, String conditionOrOperationId, String conditionOrOperationType) {
+		this(type, conditionOrOperationId, conditionOrOperationType, null, null, true);
 	}
 	
-	public ConditionOrOperationViewData(int type, String conditionOrOperationType) {
-		this(type, conditionOrOperationType, null, null, true);
+	public ConditionOrOperationViewData(int type, String conditionOrOperationId, String conditionOrOperationType, String mainText) {
+		this(type, conditionOrOperationId, conditionOrOperationType, mainText, null, true);
 	}
 	
-	public ConditionOrOperationViewData(int type, String conditionOrOperationType, String mainText) {
-		this(type, conditionOrOperationType, mainText, null, true);
+	public ConditionOrOperationViewData(int type, String conditionOrOperationId, String conditionOrOperationType, String mainText, String typeText) {
+		this(type, conditionOrOperationId, conditionOrOperationType, mainText, typeText, true);
 	}
 	
-	public ConditionOrOperationViewData(int type, String conditionOrOperationType, String mainText, String typeText) {
-		this(type, conditionOrOperationType, mainText, typeText, true);
-	}
-	
-	public ConditionOrOperationViewData(int type, String conditionOrOperationType, String mainText, String typeText, boolean dragHandleVisible) {
+	public ConditionOrOperationViewData(int type, String conditionOrOperationId, String conditionOrOperationType, String mainText, String typeText, boolean dragHandleVisible) {
 		this.type = type;
+		this.conditionOrOperationId = conditionOrOperationId;
 		this.conditionOrOperationType = conditionOrOperationType;
 		this.mainText = mainText;
 		this.typeText = typeText;
 		this.dragHandleVisible = dragHandleVisible;
+	}
+	
+	
+	public String getConditionOrOperationId() {
+		return conditionOrOperationId;
+	}
+	
+	public void setConditionOrOperationId(String conditionOrOperationId) {
+		this.conditionOrOperationId = conditionOrOperationId;
 	}
 	
 	public int getType() {

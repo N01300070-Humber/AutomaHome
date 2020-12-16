b0e1692842d21f7d661328e2cb1daa465e5ac365
Calvin Abel <n01300070@humbermail.ca>
2020-12-15 22:16:19 -0500

    Fixed the obtaining of extras in EditDevicesActivity

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/edit/EditDevicesActivity.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/edit/EditDevicesActivity.java
@@ -8,6 +8,7 @@ import androidx.lifecycle.Observer;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -29,142 +30,141 @@ import ca.humbermail.n01300070.automahome.data.model.Device;
 import ca.humbermail.n01300070.automahome.data.model.DeviceOrTaskButtonData;
 import ca.humbermail.n01300070.automahome.ui.CustomActivity;
 
-public class EditDevicesActivity extends CustomActivity
-{
-    public static String EXTRA_DEVICE_ID = "deviceId";
-    public static String EXTRA_DEVICE_NAME = "deviceName";
-    public static String EXTRA_DEVICE_CATEGORY = "category";
-
-    private static final String DEFAULT_NAME = "Untitled Device";
-
-    private Button saveButton;
-    private Button deleteButton;
-    private Fragment fragment;
-    private FavoriteSelectView favoriteSelectView;
-    private Spinner roomSpinner;
-    private Spinner roomSpinner2;
-    private TextView roomLocationHeader;
-    private TextView roomLocationHeader2;
-    private String deviceType;
-    private TextInputEditText nameEditText;
-
-
-    private RealtimeDatabaseDataSource realtimeDatabaseDataSource;
-    private String deviceId;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_devices);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        saveButton = findViewById(R.id.button_editDevice_save);
-        deleteButton = findViewById(R.id.button_editDevice_delete);
-        roomSpinner = findViewById(R.id.spinner_editDevice);
-        roomSpinner2 = findViewById(R.id.spinner_editDevice_2);
-        roomLocationHeader = findViewById(R.id.textView_deviceLocation_editDevice);
-        roomLocationHeader2 = findViewById(R.id.textView_editDevice_deviceLocation2);
-        favoriteSelectView = findViewById(R.id.favoriteSelectView_editDevice);
-        nameEditText = findViewById(R.id.editText_editDevice);
-
-        deviceType = getIntent().getExtras().getString(DeviceOrTaskButtonData.ARG_DEVICE);
-        favoriteSelectView.setAutoCompleteLabels(generateCategoryList());
-
-        setRealtimeDatabaseDataSource(new RealtimeDatabaseDataSource());
-        realtimeDatabaseDataSource = getRealtimeDatabaseDataSource();
-        realtimeDatabaseDataSource.setCurrentHomeId(
-                getSharedPreferences(PreferenceKeys.KEY_SESSION, Context.MODE_PRIVATE)
-                .getString(PreferenceKeys.KEY_SESSION_SELECTED_HOME,"")
-        );
-
-        // Get Extras
-        Bundle bundle = getIntent().getExtras();
-        if (bundle == null) {
-            deviceId = realtimeDatabaseDataSource.addDevice(DEFAULT_NAME,"","","");
-            nameEditText.setText(DEFAULT_NAME);
-
-        } else {
-            deviceId = bundle.getString(EXTRA_DEVICE_ID);
-            nameEditText.setText(bundle.getString(EXTRA_DEVICE_NAME));
-
-            String category = bundle.getString(EXTRA_DEVICE_CATEGORY);
-            if (category != null && !category.isEmpty()) {
-                favoriteSelectView.setChecked(true);
-                favoriteSelectView.setText(category);
-            }
-        }
-
-        switch(deviceType){
-            case DeviceOrTaskButtonData.DEVICE_LIGHTS:
-                fragment = new EditLightFragment();
-                break;
-            case DeviceOrTaskButtonData.DEVICE_MOVEMENT_SENSOR:
-                roomLocationHeader.setText(getString(R.string.side_device_location, "A"));
-                roomLocationHeader2.setText(getString(R.string.side_device_location, "B"));
-                roomLocationHeader2.setVisibility(View.VISIBLE);
-                roomSpinner2.setVisibility(View.VISIBLE);
-                fragment = new EditMovementSensorFragment();
-                break;
-            case DeviceOrTaskButtonData.DEVICE_THERMOSTAT:
-                fragment = new EditThermostatFragment();
-                break;
-            default:
-                Toast.makeText(getApplicationContext(), "Error: failed to get device type", Toast.LENGTH_SHORT).show();
-                finish();
-                return;
-        }
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_editDevice, fragment).commit();
-    }
-
-    public void deleteButtonClicked(View view) {
-        realtimeDatabaseDataSource.removeTask(deviceId);
-        finish();
-    }
+public class EditDevicesActivity extends CustomActivity {
+	public static String EXTRA_DEVICE_ID = "deviceId";
+	public static String EXTRA_DEVICE_NAME = "deviceName";
+	public static String EXTRA_DEVICE_CATEGORY = "category";
+	
+	private static final String DEFAULT_NAME = "Untitled Device";
+	
+	private Button saveButton;
+	private Button deleteButton;
+	private Fragment fragment;
+	private FavoriteSelectView favoriteSelectView;
+	private Spinner roomSpinner;
+	private Spinner roomSpinner2;
+	private TextView roomLocationHeader;
+	private TextView roomLocationHeader2;
+	private String deviceType;
+	private TextInputEditText nameEditText;
+	
+	
+	private RealtimeDatabaseDataSource realtimeDatabaseDataSource;
+	private String deviceId;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_devices);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		saveButton = findViewById(R.id.button_editDevice_save);
+		deleteButton = findViewById(R.id.button_editDevice_delete);
+		roomSpinner = findViewById(R.id.spinner_editDevice);
+		roomSpinner2 = findViewById(R.id.spinner_editDevice_2);
+		roomLocationHeader = findViewById(R.id.textView_deviceLocation_editDevice);
+		roomLocationHeader2 = findViewById(R.id.textView_editDevice_deviceLocation2);
+		favoriteSelectView = findViewById(R.id.favoriteSelectView_editDevice);
+		nameEditText = findViewById(R.id.editText_editDevice);
+		
+		favoriteSelectView.setAutoCompleteLabels(generateCategoryList());
+		
+		setRealtimeDatabaseDataSource(new RealtimeDatabaseDataSource());
+		realtimeDatabaseDataSource = getRealtimeDatabaseDataSource();
+		realtimeDatabaseDataSource.setCurrentHomeId(
+				getSharedPreferences(PreferenceKeys.KEY_SESSION, Context.MODE_PRIVATE)
+						.getString(PreferenceKeys.KEY_SESSION_SELECTED_HOME, "")
+		);
+		
+		// Get Extras (Bundle must not be null!)
+		Bundle bundle = getIntent().getExtras();
+		deviceId = bundle.getString(EXTRA_DEVICE_ID);
+		nameEditText.setText(bundle.getString(EXTRA_DEVICE_NAME));
+		deviceType = getIntent().getExtras().getString(DeviceOrTaskButtonData.ARG_DEVICE);
+		if (deviceId == null) {
+			deviceId = realtimeDatabaseDataSource.addDevice(DEFAULT_NAME, deviceType, "", "");
+		}
+		
+		String category = bundle.getString(EXTRA_DEVICE_CATEGORY);
+		if (category != null && !category.isEmpty()) {
+			favoriteSelectView.setChecked(true);
+			favoriteSelectView.setText(category);
+		}
+		
+		switch (deviceType) {
+			case DeviceOrTaskButtonData.DEVICE_LIGHTS:
+				fragment = new EditLightFragment();
+				break;
+			case DeviceOrTaskButtonData.DEVICE_MOVEMENT_SENSOR:
+				roomLocationHeader.setText(getString(R.string.side_device_location, "A"));
+				roomLocationHeader2.setText(getString(R.string.side_device_location, "B"));
+				roomLocationHeader2.setVisibility(View.VISIBLE);
+				roomSpinner2.setVisibility(View.VISIBLE);
+				fragment = new EditMovementSensorFragment();
+				break;
+			case DeviceOrTaskButtonData.DEVICE_THERMOSTAT:
+				fragment = new EditThermostatFragment();
+				break;
+			default:
+				Toast.makeText(getApplicationContext(), "Error: failed to get device type", Toast.LENGTH_SHORT).show();
+				finish();
+				return;
+		}
+		getSupportFragmentManager().beginTransaction().add(R.id.fragment_editDevice, fragment).commit();
+	}
+	
+	public void deleteButtonClicked(View view) {
+		realtimeDatabaseDataSource.removeTask(deviceId);
+		finish();
+	}
 
 //    public void discardButtonClicked(View view) {
 //        //TODO data handling
 //        setResult(Activity.RESULT_CANCELED);
 //        finish();
 //    }
-
-    public void saveButtonClicked(View view) {
-        Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show(); // TODO: Remove placeholder toast
-        saveName();
-        saveFavoriteCategory();
-        setResult(Activity.RESULT_OK);
-        finish();
-    }
-    private void saveName() {
-        realtimeDatabaseDataSource.updateDeviceName(deviceId, Objects.requireNonNull(nameEditText.getText().toString()));
-    }
-
-    private void saveFavoriteCategory() {
-        if(favoriteSelectView.isChecked()) {
-            realtimeDatabaseDataSource.updateDeviceCategory(deviceId,favoriteSelectView.getText());
-        } else {
-            realtimeDatabaseDataSource.updateDeviceCategory(deviceId,"");
-        }
-    }
-    private ArrayList<String> generateCategoryList() {
-        int numCategories = 6;
-        ArrayList<String> categoryList = new ArrayList<>(numCategories);
-        
-        for (int i = 0; i < numCategories; i++) {
-            categoryList.add("Category " + (i + 1));
-        }
-        
-        return categoryList;
-    }
-    
-    /**
-     * Handles back button onClick event
-     * @param item non-null MenuItem
-     * @return Boolean
-     */
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        setResult(Activity.RESULT_CANCELED);
-        finish();
-        return true;
-    }
+	
+	public void saveButtonClicked(View view) {
+		Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show(); // TODO: Remove placeholder toast
+		saveName();
+		saveFavoriteCategory();
+		setResult(Activity.RESULT_OK);
+		finish();
+	}
+	
+	private void saveName() {
+		realtimeDatabaseDataSource.updateDeviceName(deviceId, Objects.requireNonNull(nameEditText.getText().toString()));
+	}
+	
+	private void saveFavoriteCategory() {
+		if (favoriteSelectView.isChecked()) {
+			realtimeDatabaseDataSource.updateDeviceCategory(deviceId, favoriteSelectView.getText());
+		} else {
+			realtimeDatabaseDataSource.updateDeviceCategory(deviceId, "");
+		}
+	}
+	
+	private ArrayList<String> generateCategoryList() {
+		int numCategories = 6;
+		ArrayList<String> categoryList = new ArrayList<>(numCategories);
+		
+		for (int i = 0; i < numCategories; i++) {
+			categoryList.add("Category " + (i + 1));
+		}
+		
+		return categoryList;
+	}
+	
+	/**
+	 * Handles back button onClick event
+	 *
+	 * @param item non-null MenuItem
+	 * @return Boolean
+	 */
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		setResult(Activity.RESULT_CANCELED);
+		finish();
+		return true;
+	}
 }

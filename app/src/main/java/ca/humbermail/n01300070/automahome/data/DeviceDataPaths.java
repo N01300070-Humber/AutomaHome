c77dd030c8839326b97be438dc9a57e18c9eb644
Calvin Abel <n01300070@humbermail.ca>
2021-03-18 16:19:39 -0400

    Added comments to each key path in DeviceDataPaths to describe the expected data type of the value found there

--- app/src/main/java/ca/humbermail/n01300070/automahome/data/DeviceDataPaths.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/data/DeviceDataPaths.java
@@ -1,16 +1,16 @@
 package ca.humbermail.n01300070.automahome.data;
 
 public class DeviceDataPaths {
-	public static final String LIGHT_RED_INTENSITY = "red";
-	public static final String LIGHT_GREEN_INTENSITY = "green";
-	public static final String LIGHT_BLUE_INTENSITY = "blue";
+	public static final String LIGHT_RED_INTENSITY = "red"; //Integer: range 0-255
+	public static final String LIGHT_GREEN_INTENSITY = "green"; //Integer: range 0-255
+	public static final String LIGHT_BLUE_INTENSITY = "blue"; //Integer: range 0-255
 	
-	public static final String MOVEMENT_LOG = "log";
-	public static final String MOVEMENT_ROOM2 = "room2";
+	public static final String MOVEMENT_LOG = "log"; //Map<String, Map<String, Object>>: Object could be unix time stamp (long) or direction (String)
+	public static final String MOVEMENT_ROOM2 = "room2"; //String
 	
-	public static final String THERMOSTAT_TEMPERATURE = "temperature";
-	public static final String THERMOSTAT_HUMIDITY = "humidity";
-	public static final String THERMOSTAT_COMPRESSOR = "compressor";
-	public static final String THERMOSTAT_REVERSE = "reverse";
-	public static final String THERMOSTAT_FAN = "fan";
+	public static final String THERMOSTAT_TEMPERATURE = "temperature"; //Integer
+	public static final String THERMOSTAT_HUMIDITY = "humidity"; //Integer
+	public static final String THERMOSTAT_COMPRESSOR = "compressor"; //Boolean
+	public static final String THERMOSTAT_REVERSE = "reverse"; //Boolean
+	public static final String THERMOSTAT_FAN = "fan"; //Boolean
 }

c70926b7050da348a8386378cb6bdc9cefa17648
Calvin Abel <n01300070@humbermail.ca>
2021-03-18 16:15:25 -0400

    Created new data class to hold key paths for device data values

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/data/DeviceDataPaths.java
@@ -0,0 +1,16 @@
+package ca.humbermail.n01300070.automahome.data;
+
+public class DeviceDataPaths {
+	public static final String LIGHT_RED_INTENSITY = "red";
+	public static final String LIGHT_GREEN_INTENSITY = "green";
+	public static final String LIGHT_BLUE_INTENSITY = "blue";
+	
+	public static final String MOVEMENT_LOG = "log";
+	public static final String MOVEMENT_ROOM2 = "room2";
+	
+	public static final String THERMOSTAT_TEMPERATURE = "temperature";
+	public static final String THERMOSTAT_HUMIDITY = "humidity";
+	public static final String THERMOSTAT_COMPRESSOR = "compressor";
+	public static final String THERMOSTAT_REVERSE = "reverse";
+	public static final String THERMOSTAT_FAN = "fan";
+}

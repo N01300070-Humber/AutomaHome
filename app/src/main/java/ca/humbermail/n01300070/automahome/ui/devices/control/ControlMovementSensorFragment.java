f86a022760b550f629c92a42e396ee8f5933c804
Calvin Abel <n01300070@humbermail.ca>
2021-03-18 15:11:28 -0400

    Modified ControlMovementSensorFragment to read real data instead of generating placeholder data

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlMovementSensorFragment.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlMovementSensorFragment.java
@@ -2,80 +2,128 @@ package ca.humbermail.n01300070.automahome.ui.devices.control;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import ca.humbermail.n01300070.automahome.R;
+import ca.humbermail.n01300070.automahome.components.DescriptiveTextView;
 import ca.humbermail.n01300070.automahome.components.ListLinePadding;
 import ca.humbermail.n01300070.automahome.components.DescriptiveTextViewAdapter;
+import ca.humbermail.n01300070.automahome.data.LoginDataSource;
+import ca.humbermail.n01300070.automahome.data.RealtimeDatabaseDataSource;
 import ca.humbermail.n01300070.automahome.data.model.DescriptiveTextViewData;
+import ca.humbermail.n01300070.automahome.data.model.Home;
 
-public class ControlMovementSensorFragment extends Fragment
-{
-    private RecyclerView detectionLog;
-    private DescriptiveTextViewAdapter adapter;
-    private Context context;
-    private ArrayList<DescriptiveTextViewData> logViewDataList;
+public class ControlMovementSensorFragment extends Fragment {
+	private Context context;
 
-    public ControlMovementSensorFragment()
-    {
-        // Required empty public constructor
-    }
+	private RecyclerView detectionLog;
+	private DescriptiveTextViewAdapter adapter;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_control_movement_sensor, container, false);
-        context = getActivity().getApplicationContext();
-
-        detectionLog = root.findViewById(R.id.recyclerView_control_movementSensor);
-
-        logViewDataList = generateLogViewDataList();
-        adapter = new DescriptiveTextViewAdapter(context, logViewDataList);
-
-        detectionLog.setLayoutManager(new LinearLayoutManager(context));
-        detectionLog.setAdapter(adapter);
-
-        detectionLog.addItemDecoration(new ListLinePadding((int) context.getResources().getDimension(R.dimen.recycler_divider_space)));
-        detectionLog.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
-
-        return root;
-    }
-
-
-    //TODO replace with method that will receive real data
-    private ArrayList<DescriptiveTextViewData> generateLogViewDataList() {
-        ArrayList<DescriptiveTextViewData> logDataList = new ArrayList<>();
-        String[] mainTextArray = {"Room 1 ➜ Room 2", "Room 2 ➜ Room 1"};
-
-        /*String[] timeTextArray = {"Today 2:40pm", "Today 8:04am", "Yesterday 2:25pm",
-                                        "Yesterday 8:07am", "Wednesday 2:43pm",
-                                    "Wednesday 7:58am", "Tuesday 2:36pm", "Tuesday 8:02am"};*/
-
-        //Modified it to where it shows the real time
-        SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
-        String[] timeTextArray = {sdf.format(new Date())};
-
-        for(int i = 0; i < timeTextArray.length; i++)
-        {
-            DescriptiveTextViewData logData = new DescriptiveTextViewData();
-            logData.setMainText(mainTextArray[i % 2]);
-            logData.setDescriptionText(timeTextArray[i]);
-            logDataList.add(logData);
-        }
-        return logDataList;
-    }
-
+	private ArrayList<DescriptiveTextViewData> logViewDataList;
+	private String deviceId;
+	private ControlDevicesActivity controlDevicesActivity;
+	
+	private RealtimeDatabaseDataSource realtimeDatabaseDataSource;
+	
+	public ControlMovementSensorFragment() {
+		// Required empty public constructor
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View root = inflater.inflate(R.layout.fragment_control_movement_sensor, container, false);
+		context = getActivity().getApplicationContext();
+		controlDevicesActivity = (ControlDevicesActivity) getActivity();
+		
+		deviceId = controlDevicesActivity.getDeviceId();
+		
+		realtimeDatabaseDataSource = controlDevicesActivity.getRealtimeDatabaseDataSource();
+		
+		detectionLog = root.findViewById(R.id.recyclerView_control_movementSensor);
+		
+		logViewDataList = new ArrayList<>();
+		adapter = new DescriptiveTextViewAdapter(context, logViewDataList);
+		
+		detectionLog.setLayoutManager(new LinearLayoutManager(context));
+		detectionLog.setAdapter(adapter);
+		
+		detectionLog.addItemDecoration(new ListLinePadding((int) context.getResources().getDimension(R.dimen.recycler_divider_space)));
+		detectionLog.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
+		
+		setOnDeviceDataValuesChangeListener();
+		
+		return root;
+	}
+	
+	
+	private void setOnDeviceDataValuesChangeListener() {
+		Log.d("ControlMovementSensor", "setOnDeviceDataValuesChangeListener called");
+		realtimeDatabaseDataSource.onDeviceDataValueChange(deviceId, null).observe(getViewLifecycleOwner(), new Observer<Object>() {
+			@Override
+			public void onChanged(Object object) {
+				Log.d("ControlMovementSensor","Device data values changed");
+				
+				Map< String, Map<String, Object> > stringMapMap;
+				
+				logViewDataList.clear();
+				if (!(object instanceof Map)) {
+					Log.d("ControlMovementSensor","Device data values object is null");
+					logViewDataList.add(new DescriptiveTextViewData("No movement data found", null)); // TODO: Replace hard coded strings with string values from strings.xml
+					return;
+				}
+				
+				stringMapMap = (Map< String, Map<String, Object> >) object;
+				
+				for (Map.Entry<String, Map<String, Object>> stringMapEntry : stringMapMap.entrySet()) {
+					DescriptiveTextViewData descriptiveTextViewData = new DescriptiveTextViewData();
+					String direction = (String) stringMapEntry.getValue().get("direction");
+					long timestamp = (long) stringMapEntry.getValue().get("timestamp");
+					Date date = new Date(timestamp * 1000L);
+					SimpleDateFormat simpleWeekDayFormat = new SimpleDateFormat("EEEE");
+					SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("h:mm a");
+					
+					// TODO: Replace hard coded strings with string values from strings.xml
+					switch (direction) {
+						case "left":
+							descriptiveTextViewData.setMainText("Room 1 ➜ Room 2");
+							break;
+						case "right":
+							descriptiveTextViewData.setMainText("Room 2 ➜ Room 1");
+							break;
+						default:
+							descriptiveTextViewData.setMainText("Unknown Direction");
+					}
+					
+					descriptiveTextViewData.setDescriptionText(
+							simpleWeekDayFormat.format(date) +
+							" at " +
+							simpleTimeFormat.format(date)
+					);
+					
+					logViewDataList.add(descriptiveTextViewData);
+				}
+				
+				adapter.notifyDataSetChanged();
+			}
+		});
+	}
 }
 

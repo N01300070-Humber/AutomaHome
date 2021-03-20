7310908eae0e53ca4cf71ee50695b160e31432cb
Calvin Abel <n01300070@humbermail.ca>
2021-03-19 20:26:24 -0400

    Added functions to convert a double number between celsius and fahrenheit

---
+++ app/src/main/java/ca/humbermail/n01300070/automahome/utils/Convert.java
@@ -0,0 +1,11 @@
+package ca.humbermail.n01300070.automahome.utils;
+
+public class Convert {
+	
+	public static double celsiusToFahrenheit(double celsius) {
+		return celsius * 9 / 5 + 32;
+	}
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		return (fahrenheit - 32) * 9 / 5;
+	}
+}

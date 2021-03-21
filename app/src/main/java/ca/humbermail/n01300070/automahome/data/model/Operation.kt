13c2e4f4105ea09ea05c901e672fbcae3b14d182
Calvin Abel <n01300070@humbermail.ca>
2021-03-21 16:18:07 -0400

    migrated from gradle plugin kotlin-android-extensions to kotlin-parcelize

--- app/src/main/java/ca/humbermail/n01300070/automahome/data/model/Operation.kt
+++ app/src/main/java/ca/humbermail/n01300070/automahome/data/model/Operation.kt
@@ -1,7 +1,7 @@
 package ca.humbermail.n01300070.automahome.data.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Operation(

40b8a0891b0ff4b0ebd97a43ce971f3c6cbe9dbb
Calvin Abel <n01300070@humbermail.ca>
2021-03-20 23:05:11 -0400

    Added database code to ControlLightFragment to handle reading and writing the colour

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlLightFragment.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlLightFragment.java
@@ -5,15 +5,20 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.ServerValue;
 
 import java.util.Map;
 
@@ -58,6 +63,8 @@ public class ControlLightFragment extends Fragment {
 		colourPreview = root.findViewById(R.id.colourPreview_controlLight);
 		
 		setOnDatabaseColourChangeListener();
+		setOnColourViewSelectionChangeListener();
+		setOnInputEditTextEditorActionListener();
 		
 		return root;
 	}
@@ -75,9 +82,9 @@ public class ControlLightFragment extends Fragment {
 				
 				if (object instanceof Map) {
 					Map<String, Long> deviceData = (Map<String, Long>) object;
-					intensityRed = deviceData.get(DeviceDataPaths.INTENSITY_RED_LED).intValue();
-					intensityGreen = deviceData.get(DeviceDataPaths.INTENSITY_GREEN_LED).intValue();
-					intensityBlue = deviceData.get(DeviceDataPaths.INTENSITY_BLUE_LED).intValue();
+					intensityRed = deviceData.get(DeviceDataPaths.LIGHT_INTENSITY_RED).intValue();
+					intensityGreen = deviceData.get(DeviceDataPaths.LIGHT_INTENSITY_GREEN).intValue();
+					intensityBlue = deviceData.get(DeviceDataPaths.LIGHT_INTENSITY_BLUE).intValue();
 				} else {
 					Log.d("ControlLight", "Device data values object is null");
 					intensityRed = 0;
@@ -91,5 +98,64 @@ public class ControlLightFragment extends Fragment {
 				colourPickerView.setInitialColor(colour);
 			}
 		});
+	}
+	
+	private void setOnColourViewSelectionChangeListener() {
+		colourPickerView.subscribe(new ColorObserver() {
+			@Override
+			public void onColor(int colour, boolean fromUser, boolean shouldPropagate) {
+				inputEditText.setText(String.format("%06X", (0xFFFFFF & colour)));
+				
+				if (fromUser) {
+					colourPreview.setBackground(new ColorDrawable(colour));
+					
+					if (shouldPropagate) {
+						setDatabaseColour(colour);
+					}
+				}
+			}
+		});
+	}
+	
+	private void setOnInputEditTextEditorActionListener() {
+		inputEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_DONE) {
+					int colour;
+					StringBuilder hexString;
+					
+					hexString = new StringBuilder(textView.getText().toString());
+					if (hexString.length() < 6) {
+						while (hexString.length() < 6) {
+							hexString.append('0');
+						}
+						textView.setText(hexString.toString());
+					}
+					hexString.insert(0,'#');
+					colour = Color.parseColor(hexString.toString());
+					
+					colourPreview.setBackground(new ColorDrawable(colour));
+					colourPickerView.setInitialColor(colour);
+					setDatabaseColour(colour);
+					
+					((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE))
+							.hideSoftInputFromWindow(
+									controlDevicesActivity.getCurrentFocus().getWindowToken(),
+									InputMethodManager.HIDE_NOT_ALWAYS
+							);
+					textInputLayout.clearFocus();
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+	
+	private void setDatabaseColour(int colour) {
+		realtimeDatabaseDataSource.setDeviceData(deviceId, DeviceDataPaths.LIGHT_INTENSITY_RED, (colour & 0x00FF0000) >> 16);
+		realtimeDatabaseDataSource.setDeviceData(deviceId, DeviceDataPaths.LIGHT_INTENSITY_GREEN, (colour & 0x0000FF00) >> 8 );
+		realtimeDatabaseDataSource.setDeviceData(deviceId, DeviceDataPaths.LIGHT_INTENSITY_BLUE, colour & 0x000000FF);
+		realtimeDatabaseDataSource.setDeviceData(deviceId, DeviceDataPaths.LIGHT_TIMESTAMP, ServerValue.TIMESTAMP);
 	}
 }

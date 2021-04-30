709670a748a22facfc3951f18d345528e71ca530
Calvin Abel <n01300070@humbermail.ca>
2021-04-30 14:44:16 -0400

    Replaced the colour handling components in ControlLightFragment with the new ColourPickerView

--- app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlLightFragment.java
+++ app/src/main/java/ca/humbermail/n01300070/automahome/ui/devices/control/ControlLightFragment.java
@@ -2,41 +2,31 @@ package ca.humbermail.n01300070.automahome.ui.devices.control;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.ServerValue;
 
 import java.util.Map;
 
 import ca.humbermail.n01300070.automahome.R;
+import ca.humbermail.n01300070.automahome.components.ColourPickerView;
 import ca.humbermail.n01300070.automahome.data.DeviceDataPaths;
 import ca.humbermail.n01300070.automahome.data.RealtimeDatabaseDataSource;
-import top.defaults.colorpicker.ColorObserver;
-import top.defaults.colorpicker.ColorPickerView;
 
 public class ControlLightFragment extends Fragment {
 	private Context context;
 	private ControlDevicesActivity controlDevicesActivity;
 	private String deviceId;
 	
-	private TextInputLayout textInputLayout;
-	private TextInputEditText inputEditText;
-	private ColorPickerView colourPickerView;
-	private View colourPreview;
+	private ColourPickerView colourPickerView;
 	
 	private RealtimeDatabaseDataSource realtimeDatabaseDataSource;
 	
@@ -57,25 +47,26 @@ public class ControlLightFragment extends Fragment {
 		
 		realtimeDatabaseDataSource = controlDevicesActivity.getRealtimeDatabaseDataSource();
 		
-		textInputLayout = root.findViewById(R.id.textInputLayout_controlLight);
-		inputEditText = root.findViewById(R.id.inputEditText_controlLight);
-		colourPickerView = root.findViewById(R.id.colourPicker_controlLight);
-		colourPreview = root.findViewById(R.id.colourPreview_controlLight);
-		
-		setOnDatabaseColourChangeListener();
-		setOnColourViewSelectionChangeListener();
-		setOnInputEditTextEditorActionListener();
-		
-		return root;
-	}
-	
-	private void setOnDatabaseColourChangeListener() {
-		Log.d("ControlLight", "setOnColourChangeListener called");
+		colourPickerView = root.findViewById(R.id.colourPickerView_controlLight);
 		
 		realtimeDatabaseDataSource.onDeviceDataValueChange(deviceId, null, true).observe(getViewLifecycleOwner(), new Observer<Object>() {
 			@Override
 			public void onChanged(Object object) {
-				int colour;
+				OnDatabaseColourChanged(object);
+			}
+		});
+		colourPickerView.addOnNumberChangeListener(new ColourPickerView.OnColourChangeListener() {
+			@Override
+			public void onColourChanged(ColourPickerView colourPickerView, int colour, boolean fromTextBox, boolean fromUser, boolean shouldPropagate) {
+				OnColourViewPickerColourChanged(colour, fromTextBox, fromUser, shouldPropagate);
+			}
+		});
+		
+		return root;
+	}
+	
+	private void OnDatabaseColourChanged(Object object) {
+		int colour;
 				int intensityRed;
 				int intensityGreen;
 				int intensityBlue;
@@ -116,62 +107,24 @@ public class ControlLightFragment extends Fragment {
 				
 				colour = Color.rgb(intensityRed, intensityGreen, intensityBlue);
 				
-				colourPreview.setBackground(new ColorDrawable(colour));
-				colourPickerView.setInitialColor(colour);
-			}
-		});
+				colourPickerView.setColour(colour);
 	}
 	
-	private void setOnColourViewSelectionChangeListener() {
-		colourPickerView.subscribe(new ColorObserver() {
-			@Override
-			public void onColor(int colour, boolean fromUser, boolean shouldPropagate) {
-				inputEditText.setText(String.format("%06X", (0xFFFFFF & colour)));
-				
-				if (fromUser) {
-					colourPreview.setBackground(new ColorDrawable(colour));
-					
-					if (shouldPropagate) {
-						setDatabaseColour(colour);
-					}
-				}
+	private void OnColourViewPickerColourChanged(int colour, boolean fromTextBox, boolean fromUser, boolean shouldPropagate) {
+		if (fromUser) {
+			if (shouldPropagate) {
+				setDatabaseColour(colour);
 			}
-		});
-	}
-	
-	private void setOnInputEditTextEditorActionListener() {
-		inputEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-			@Override
-			public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
-				if (actionId == EditorInfo.IME_ACTION_DONE) {
-					int colour;
-					StringBuilder hexString;
-					
-					hexString = new StringBuilder(textView.getText().toString());
-					if (hexString.length() < 6) {
-						while (hexString.length() < 6) {
-							hexString.append('0');
-						}
-						textView.setText(hexString.toString());
-					}
-					hexString.insert(0,'#');
-					colour = Color.parseColor(hexString.toString());
-					
-					colourPreview.setBackground(new ColorDrawable(colour));
-					colourPickerView.setInitialColor(colour);
-					setDatabaseColour(colour);
-					
-					((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE))
-							.hideSoftInputFromWindow(
-									controlDevicesActivity.getCurrentFocus().getWindowToken(),
-									InputMethodManager.HIDE_NOT_ALWAYS
-							);
-					textInputLayout.clearFocus();
-					return true;
-				}
-				return false;
+			
+			if (fromTextBox) {
+				((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE))
+						.hideSoftInputFromWindow(
+								controlDevicesActivity.getCurrentFocus().getWindowToken(),
+								InputMethodManager.HIDE_NOT_ALWAYS
+						);
+				colourPickerView.clearFocus();
 			}
-		});
+		}
 	}
 	
 	private void setDatabaseColour(int colour) {
